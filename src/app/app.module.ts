d1391aa2f310f564c8fa988f54880d6045d7daeb
unknown <deepak.verma@live.in>
2020-03-18 22:16:41 +0530

    added Authentication'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from "@angular/fire";
-import { AngularFirestoreModule } from "@angular/fire/firestore";
-import { FormsModule } from "@angular/forms";
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
- 
+
 import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
@@ -13,22 +13,41 @@ import { EmployeeComponent } from './employees/employee/employee.component';
 import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
 import { EmployeeService } from './shared/employee.service';
 
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AuthService } from './services/auth.service';
+import { Router } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { SuperSecretComponent } from './super-secret/super-secret.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesComponent,
     EmployeeComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    UserProfileComponent,
+    SuperSecretComponent
+
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
+    AngularFireAuthModule,
+    AppRoutingModule,
+
     FormsModule,
     BrowserAnimationsModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
+
   ],
-  providers: [EmployeeService],
+  providers: [
+    EmployeeService,
+    AuthService,
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
